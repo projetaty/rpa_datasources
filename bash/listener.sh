54523088e21a87874eeb4d28d508ff8062b1ad14
samurai <projetaty@gmail.com>
2021-01-11 07:29:49 -0200

    bash job tests done.

--- bash/listener.sh
+++ bash/listener.sh
@@ -2,12 +2,6 @@
 
 ### BEGIN INIT INFO
 # Provides:             Notify inbound folder event service
-# Required-Start:       $remote_fs $syslog
-# Required-Stop:        $remote_fs $syslog
-# Should-Start:         $network
-# Should-Stop:          $network
-# Default-Start:        2 3 4 5
-# Default-Stop:         0 1 6
 # Short-Description:    Script part of single RPA
 # Author:          		Sandro Regis Cardoso | Software Eng.
 ### END INIT INFO
@@ -18,8 +12,11 @@ bash_log_dir=$parent_dir/log/bash
 log_file=$bash_log_dir/datasource_notify.log
 dir_inbound=$parent_dir/inbound
 inotify=/usr/bin/inotifywait
-actions=CREATE,MOVED_TO,DELETE
+actions=CREATE,MOVED_TO,DELETE,ACCESS
 xmlfile="xml"
+xlsfile="xls"
+csvfile="csv"
+txtfile="txt"
 
 set -e
 
@@ -27,23 +24,39 @@ if ! [[ -e $bash_log_dir ]]
 	then
 		mkdir -p $bash_log_dir;
 fi
+
 if ! [[ -e $log_file ]]
 	then
 		touch $log_file
 fi
 
-#TODO: Test multiples files inbounding
 inbound_monitor() {
 	while $inotify --recursive --event $actions "$dir_inbound"
 		do
-			sleep 3
-			file_name=$(ls $dir_inbound)
-			file_extension=${file_name##*.}
-			if [ $file_extension == $xmlfile ]
-				then
-					echo "Action : Call parser to $file_extension file"
-					rm -f $dir_inbound"/"$file_name;
-			fi
+			sleep 0.7
+			FILES=$(ls $dir_inbound)
+			for file_name in $FILES
+				do
+				  	file_extension=${file_name##*.}
+					if [ $file_extension == $xmlfile ]
+						then
+							echo "Action : Call parser to $file_extension file"
+							rm -f $dir_inbound"/"$file_name;
+					
+					elif [ $file_extension == $xlsfile ]
+						then
+							echo "Action : Call parser to $file_extension file"
+							rm -f $dir_inbound"/"$file_name;
+					
+					elif [ $file_extension == $csvfile ]
+						then
+							echo "Action : Call parser to $file_extension file"
+							rm -f $dir_inbound"/"$file_name;
+					
+					else
+						echo "Action : Send mail file extension $file_extension not supported."
+					fi
+				done
 		done
 }
 
